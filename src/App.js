050a6c8c436c9999fc0d88cb76f18bbe8ae4f8e7
郭超超 <chaochao.guo@hand-china.com>
2020-04-23 11:22:34 +0800

    '添加图表，选择框'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+
+import "./App.css";
+import GlobalStats from "./components/GlobalStats";
+import CountriesChart from "./components/CountriesChart";
+import SelectDataKey from "./components/SelectDataKey";
+
+// const BASE_URL = "https://corona.lmao.ninja";
+import { useCoronaAPI } from "./hooks/useCoronaAPI";
 
 function App() {
+  // const [globalStats, setGlobalStats] = useState({});
+  // const [countries, setCountries] = useState([]);
+  // const [key, setKey] = useState("cases");
+
+  // useEffect(() => {
+  //   const fetchGlobalStats = async () => {
+  //     const response = await fetch(`${BASE_URL}/v2/all`);
+  //     const data = await response.json();
+  //     setGlobalStats(data);
+  //   };
+
+  //   fetchGlobalStats();
+  //   const intervalId = setInterval(fetchGlobalStats, 5000);
+  const globalStats = useCoronaAPI("/v2/all", {
+    initialData: {},
+    refetchInterval: 5000,
+  });
+
+  //   return () => clearInterval(intervalId);
+  // }, []);
+
+  // useEffect(() => {
+  //   const fetchCountries = async () => {
+  //     const response = await fetch(`${BASE_URL}/v2/countries?sort=${key}`);
+  //     const data = await response.json();
+  //     setCountries(data.slice(0, 10));
+  //   };
+
+  //   fetchCountries();
+  // }, [key]);
+  const [key, setKey] = useState("cases");
+  const countries = useCoronaAPI(`/v2/countries?sort=${key}`, {
+    initialData: [],
+    converter: (data) => data.slice(0, 10),
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>COVID-19</h1>
+      <GlobalStats stats={globalStats} />
+      <SelectDataKey onChange={(e) => setKey(e.target.value)} />
+      <CountriesChart data={countries} dataKey={key} />
     </div>
   );
 }
---
+++ src/hooks/useCoronaAPI.js
@@ -0,0 +1,30 @@
+// import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
+
+const BASE_URL = "https://corona.lmao.ninja";
+
+export function useCoronaAPI(
+    path,
+    { initialData = null, converter = (data) => data, refetchInterval = null }
+) {
+    const [data, setData] = useState(initialData);
+    const convertData = useCallback(converter, []);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`${BASE_URL}${path}`);
+            const data = await response.json();
+            // setData(converter(data));
+            setData(convertData(data));
+        };
+        fetchData();
+
+        if (refetchInterval) {
+            const intervalId = setInterval(fetchData, refetchInterval);
+            return () => clearInterval(intervalId);
+        }
+        // }, [converter, path, refetchInterval]);
+    }, [convertData, path, refetchInterval]);
+
+    return data;
+}
